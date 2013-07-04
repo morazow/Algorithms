cc41b3ddbb66455209621bdb58de5a52e617984d
Muhammet Orazov <m.orazow@gmail.com>
2013-07-05 00:06:57 +0100

    simple implementation of Karatsuba algorithm using string. I need to improve it to use vector and needs a lot of testing

---
+++ Karatsuba/gen_test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int N;
+char buffer[100001];
+
+int main(int argc, char* argv[]) {
+    int i, v;
+
+    if (argc < 2) {
+        printf("Usage: size of numbers\n");
+        return 0;
+    }
+
+    srand(time(NULL));
+    sscanf(argv[1], "%d", &N);
+
+    buffer[0] = '0' + (rand()%10 + 1);
+    for (i = 1; i < N; i++)
+        buffer[i] = '0' + (rand() % 10);
+    printf("%s\n", buffer);
+
+    buffer[0] = '0' + (rand()%10 + 1);
+    for (i = 1; i < N; i++)
+        buffer[i] = '0' + (rand() % 10);
+    printf("%s\n", buffer);
+
+    return 0;
+}
---
+++ Karatsuba/karatsuba.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+string removeZeros(string);
+string shift(string, size_t);
+string add(string, string);
+string sub(string, string);
+string karatsuba(string, string);
+
+
+int main() {
+    size_t N;
+    char buffer[1000001];
+
+    scanf("%s", buffer);
+    string X = string(buffer);
+    scanf("%s", buffer);
+    string Y = string(buffer);
+
+    printf("%s\n", karatsuba(X, Y).c_str());
+
+    return 0;
+}
+
+string removeZeros(string s) {
+    size_t pos;
+    for (pos = 0; pos < s.size(); pos++)
+        if (s[pos] != '0') break;
+    if (pos == s.size())
+        return "0";
+    return s.substr(pos);
+}
+
+string shift(string s, size_t n) {
+    for (int i = 0; i < n; i++)
+        s += "0";
+    return s;
+}
+
+string add(string a, string b) {
+    size_t n = a.size();
+    size_t m = b.size();
+    if (!n || !m) {
+        printf("one of strings is NULL");
+        exit(0);
+    }
+    size_t len = n < m ? m : n;
+    for (size_t i = n; i < len; i++) a = "0" + a;
+    for (size_t i = m; i < len; i++) b = "0" + b;
+
+    string res(len, '0');
+    int i, val, carry = 0;
+    for (i = len-1; i >= 0; i--) {
+        val = (carry + (a[i]-'0') + (b[i]-'0'));
+        carry = val / 10;
+        res[i] = '0' + (val%10);
+    }
+    if (carry)
+        res = "1" + res;
+
+    return removeZeros(res);
+}
+
+
+string sub(string a, string b) {
+    size_t n = a.size();
+    size_t m = b.size();
+    size_t len = n < m ? m : n;
+    for (size_t i = n; i < len; i++) a = "0" + a;
+    for (size_t i = m; i < len; i++) b = "0" + b;
+
+    string res(len, '0');
+    int carry = 0;
+    for (int i = len-1; i >= 0; i--) {
+        int val = (carry + (a[i]-'0') - (b[i]-'0'));
+        if (val < 0) {
+            val += 10;
+            carry = -1;
+        }
+        else
+            carry = 0;
+        res[i] = '0' + (val%10);
+    }
+    // take care of minus ( should not happen here )
+
+    return removeZeros(res);
+}
+
+
+string karatsuba(string X, string Y) {
+
+    size_t n = X.size();
+    size_t m = Y.size();
+    size_t N = n < m ? m : n;
+
+    if (N != 1 && N%2) {
+        N = N+1;
+    }
+
+    for (int i = n; i < N; i++) X = "0" + X;
+    for (int i = m; i < N; i++) Y = "0" + Y;
+
+    if (N <= 1) {
+        ostringstream tmp;
+        tmp << (X[0] - '0') * (Y[0] - '0');
+        return tmp.str();
+    }
+
+    string A = X.substr(0, N/2);
+    string B = X.substr(N/2);
+    string C = Y.substr(0, N/2);
+    string D = Y.substr(N/2);
+
+    string ac  = karatsuba(A, C);
+    string bd  = karatsuba(B, D);
+    string mid = karatsuba(add(A, B), add(C, D));
+
+    string term1 = shift(ac, N);
+    string term2 = shift(sub(mid, add(ac, bd)), N/2);
+    string res   = add(add(term1, term2), bd);
+
+    return res;
+}
+
