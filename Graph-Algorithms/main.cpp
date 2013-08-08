90bbfc3165044c61f098e12f22956c15990a2054
Muhammet Orazov <m.orazow@gmail.com>
2013-08-08 18:14:41 +0100

    Added driver program for Dijkstra shortest path algorithm.

--- Graph-Algorithms/main.cpp
+++ Graph-Algorithms/main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "graph.h"
 
 int main(int argc, char* argv[]) {
-    int N, u, v;
-    bool directed = true;
+    int N, u, v, d;
+    bool directed = false;
+    char buffer[1024];
 
-    fscanf(stdin, "%d", &N);
+    fgets(buffer, 1024, stdin);
+    sscanf(buffer, "%d", &N);
+
     Graph *G = new Graph(N, 0, directed);
-    while (fscanf(stdin, "%d %d", &u, &v) != EOF) {
-        u--; v--;
-        G->addEdge(u, v);
+
+    std::string line;
+    char comma;
+    while (fgets(buffer, 1024, stdin)) {
+        std::istringstream in(buffer);
+        in >> u;
+        assert(u > 0 && u <= 200);
+        u--;
+        while (in >> v >> comma >> d) {
+            assert(v > 0 && v <= 200);
+            v--;
+            G->addEdge(u, v, d);
+        }
     }
-    G->SCC();
+    G->Dijkstra(0);
     delete G;
 
     return 0;
