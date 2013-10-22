b97b800ab2026a270ffb6a3b5b2f9c2b5b0114fe
Muhammet Orazov <m.orazow@gmail.com>
2013-10-22 19:37:53 +0200

    Updated sqrt impelementation. (Fixed bugs)

--- InverseFunction/main.cpp
+++ InverseFunction/main.cpp
@@ -1,10 +1,13 @@
 #include "sqrt_newtons.h"
+#include <assert.h>
 
 int main(int argc, char* argv[]) {
 
-    for (int i = 0; i < 100; i++) {
-        printf("%d %f\n", i, sqrt_newtons((double)i));
+    for (int i = 100000; i < 100000000; i++) {
+        assert( sqrt_newtons((double)i) - sqrt((double)i) < 0.0001);
     }
+    assert( sqrt_newtons(1e-50) - sqrt(1e-50) < 0.0001);
+    assert( sqrt_newtons(1e60) - sqrt(1e60) < 0.0001);
 
     return 0;
 }
--- InverseFunction/sqrt_newtons.h
+++ InverseFunction/sqrt_newtons.h
@@ -5,10 +5,20 @@
 #include <stdio.h>
 #include <math.h>
 
-const double EPS = 1e-9;
+const double EPS = 1e-15;
 
 double sqrt_newtons(double x, double guess = 1.0) {
-    if (fabs(x - guess * guess) < EPS)
+    // assert( x > 0 );
+    /*
+     * if (fabs(x - guess * guess) < EPS)
+     * This check is problematic; for very very small
+     * numbers it will result in wrong answer, and for
+     * very large numbers it will go into infinite loop.
+     * So we need to normalize it by number (x) itself
+     * before checking.
+     *
+     */
+    if (fabs(x - guess * guess) / x < EPS)
         return guess;
     return sqrt_newtons(x, (guess + x / guess) / 2);
 }
